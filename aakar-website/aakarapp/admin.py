4874b248408ea7c94c8b0904b9dc4c628bd235d4
Himanshi Yadav <himanshi.iitb62@gmail.com>
2022-09-19 18:53:35 +0530

    added custom model

--- aakar-website/aakarapp/admin.py
+++ aakar-website/aakarapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import TaskZero
 # Register your models here.
+
+admin.site.register(TaskZero)
--- aakar-website/aakarapp/models.py
+++ aakar-website/aakarapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+class TaskZero(models.Model):
+    crid = models.CharField(max_length=10)
+    firstName = models.CharField(max_length = 200)
+    lasttName = models.CharField(max_length = 200)
+    city = models.CharField(max_length = 200)
+    phone = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.firstName
+
+
